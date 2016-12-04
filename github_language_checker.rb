18fd62dae3fd5df4257cb174bf4a44a3290a11b9
emma-ashley <bitbucket@emma-ashley.com>
2016-12-04 23:36:42 +0000

    can now contact github for number of repositories a user has

--- github_language_checker.rb
+++ github_language_checker.rb
@@ -1,8 +1,21 @@
+require 'octokit'
+
 module GithubLanguageChecker
 	class LanguageChecker
 
 		def self.check_language(name_to_search)
+
+          begin
+			repositories = Octokit.repositories(name_to_search)
+			puts "User #{name_to_search} has #{repositories.length} repos."
+		  rescue	
 			puts 'Cannot connect to Github API at this time.'
+		  end
+
 		end
 	end
 end
+
+#puts 'Please enter a github username:'
+
+#GithubLanguageChecker::LanguageChecker.check_language(gets.chomp())
--- spec/end_to_end_spec.rb
+++ spec/end_to_end_spec.rb
@@ -1,8 +1,19 @@
 require_relative '../github_language_checker.rb'
 
 describe "Github Language Checker" do
+
+  before(:each) do
+    @username = "emashliles" 
+  end
+
 	it "displays error when Github API can't be reached" do
 		expect(STDOUT).to receive(:puts).with("Cannot connect to Github API at this time.")
-		GithubLanguageChecker::LanguageChecker.check_language('emashliles')
+		allow(Octokit).to receive(:repositories).with(@username).and_raise("exception")
+		GithubLanguageChecker::LanguageChecker.check_language(@username)
+	end
+
+	it "displays the number of repos a user has" do
+		expect(STDOUT).to receive(:puts).with("User #{@username} has 27 repos.")
+		GithubLanguageChecker::LanguageChecker.check_language(@username)
 	end
 end
