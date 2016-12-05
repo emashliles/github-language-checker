f04da77a156b54e38d8de44c57438566a0713072
emma-ashley <bitbucket@emma-ashley.com>
2016-12-05 01:24:53 +0000

    now can find most frequently used language

--- github_language_checker.rb
+++ github_language_checker.rb
@@ -3,19 +3,36 @@ require 'octokit'
 module GithubLanguageChecker
 	class LanguageChecker
 
-		def self.check_language(name_to_search)
+		def check_language(name_to_search)
 
           begin
 			repositories = Octokit.repositories(name_to_search)
 			puts "User #{name_to_search} has #{repositories.length} repos."
+
+            most_used_language = find_most_frequent_langauge(repositories)
+
+			puts "It looks like their favortie language is #{most_used_language}"
 		  rescue	
 			puts 'Cannot connect to Github API at this time.'
 		  end
 
 		end
+	
+
+	def find_most_frequent_langauge(repositories)
+		langauges_used = Hash.new(0)
+
+        repositories.each { |repo| langauges_used[repo.language] += 1 }
+
+        most_used_language = langauges_used.max_by { |k, v| v }[0]
+
+        return most_used_language
 	end
+  end
 end
 
-#puts 'Please enter a github username:'
+puts 'Please enter a github username:'
 
-#GithubLanguageChecker::LanguageChecker.check_language(gets.chomp())
+language_checker = GithubLanguageChecker::LanguageChecker.new
+
+language_checker.check_language(gets.chomp) 
--- spec/end_to_end_spec.rb
+++ spec/end_to_end_spec.rb
@@ -4,16 +4,18 @@ describe "Github Language Checker" do
 
   before(:each) do
     @username = "emashliles" 
+    @language_checker = GithubLanguageChecker::LanguageChecker.new
   end
 
 	it "displays error when Github API can't be reached" do
 		expect(STDOUT).to receive(:puts).with("Cannot connect to Github API at this time.")
 		allow(Octokit).to receive(:repositories).with(@username).and_raise("exception")
-		GithubLanguageChecker::LanguageChecker.check_language(@username)
+		@language_checker.check_language(@username)
 	end
 
-	it "displays the number of repos a user has" do
+	it "displays the number of repos a user has and makes a guess at the user's favorite language" do
 		expect(STDOUT).to receive(:puts).with("User #{@username} has 27 repos.")
-		GithubLanguageChecker::LanguageChecker.check_language(@username)
+		expect(STDOUT).to receive(:puts).with("It looks like their favortie language is JavaScript")
+		@language_checker.check_language(@username)
 	end
 end
